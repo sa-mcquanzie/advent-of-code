911510b1bfc4706413008a73d629536ce0780a73
sa-mcquanzie <sa.mcquanzie@gmail.com>
2021-12-07 20:39:41 +0000

    Complete 2021 day 6

---
+++ 2021/06/02/puzzle.rb
@@ -0,0 +1,12 @@
+DAYS = 256
+
+fishes = Array.new(9, 0)
+File.read('../inputs').chomp.split(',').map(&:to_i).each { |stage| fishes[stage] += 1 }
+
+DAYS.times do
+  birthing_fish = fishes.first
+  fishes.rotate!
+  fishes[6] += birthing_fish
+end
+
+puts fishes.sum
